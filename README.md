771412604dc1c64cb44c3e718a1e2d802d89b981
theamazing0 <66532288+theamazing0@users.noreply.github.com>
2022-11-21 14:54:33 -0500

    Initial

--- README.md
+++
@@ -1,3 +0,0 @@
-# theamazing0/flask-tutorial
-This is a tutorial/template repository that can demonstrates many useful functions in a Flask webstack.
-If run, will host a basic todo application.
--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import sqlite3
+from flask import Flask, render_template
 
 app = Flask('__name__')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -7,35 +6,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def index():
-    conn = sqlite3.connect('todo.db')
-    todos = conn.execute("SELECT NAME, DESCRIPTION from TODOS").fetchall()
-    conn.close()
-    return render_template('index.html', todos=todos)
-
-
-@app.route('/savetodo', methods=['POST'])
-def savetodo():
-    name = request.form.get('name')
-    description = request.form.get('description')
-    conn = sqlite3.connect('todo.db')
-    conn.execute("INSERT INTO TODOS (NAME, DESCRIPTION) \
-        VALUES (?, ?)",
-                 (name, description))
-    conn.commit()
-    conn.close()
-    return "success"
-
-
-@app.route('/deletetodo', methods=['DELETE'])
-def deletetodo(methods=['DELETE']):
-    todoToDelete = request.form.get('todoToDelete')
-    todoToDeleteFormatted = "'" + todoToDelete + "'"
-    conn = sqlite3.connect('todo.db')
-    conn.execute("DELETE from TODOS where NAME = ?;", [todoToDelete])
-    conn.commit()
-    conn.close()
-    return "success"
-
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # ! Remove debug = True after development
